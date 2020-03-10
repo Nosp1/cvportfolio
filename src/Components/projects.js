78d6f265f55ab4e4d54d297a09109c0792e7b90a
Trym Erlend Staurheim <trymes13%student.uia.no>
2020-03-10 14:05:09 +0100

    photo

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -12,62 +12,48 @@ class Projects extends Component {
             return (
                 <div className="projects-grid">
                     {/* Project 1 */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle className="card-title" style={{
-                            color: 'white',
-                            height: '176px',
-                            background: 'black url(https://github.com/Nosp1/Dnd/raw/master/logo.png) center / cover'
-                        }}>DnD: Character Generator</CardTitle>
-                        <CardText>
-                            StoryTeller is a open-source minimalistic character-creator right in your terminal for your favorite
-                            roleplaying game. The process of creating a character can be tedious and time consuming.
-                            Storyteller allows you to have a playable character within minutes. Save your character to JSON.
-                        </CardText>
-                        <CardActions style={{textAlign: 'center' }}>
-                            <Button  colored href="https://github.com/Nosp1/Dnd" target="_blank" >Github</Button>
-                        </CardActions>
-                        <CardMenu style={{color: '#fff'}}>
-                            <IconButton name="share"/>
-                        </CardMenu>
-                    </Card>
-                    {/* Project 2*/}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle className="card-title" style={{
-                        color: 'white',
-                        height: '176px',
-                        background: 'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80) center / cover',
-                    }}> Roombooking System</CardTitle>
-                    <CardText>
-                        A complete system that allows a user to book an available room in the system.
-                        The system allows users to easily book, rebook and keep track over their bookings.
-                        Features an admin-hub for easy management of the system and emailing-services to inform your clients.
-                    </CardText>
-                    <CardActions style={{textAlign: 'center' }}>
-                        <Button colored href="https://github.com/Nosp1/Gruppe10">Github</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share"/>
-                    </CardMenu>
-                </Card>
-                    {/* Project 3 */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{
-                        color: '#fff',
-                        height: '176px',
-                        background: 'url(https://reactjs.org/logo-og.png) center / cover'
-                    }}>React Project 3</CardTitle>
-                    <CardText>
-                        Bacon ipsum dolor amet leberkas ground round biltong landjaeger, rump doner burgdoggen bresaola
-                        strip steak spare ribs sirloin cupim shoulder salami. Tail tenderloin andouille tongue sausage
-                        strip steak pork.
-                    </CardText>
-                    <CardActions style={{textAlign: 'center' }}>
-                        <Button colored>Github</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share"/>
-                    </CardMenu>
-                </Card>
+                    <div className="project-cards">
+                        <Card className="project-card" shadow={5}>
+                            <CardTitle className="card-title" style={{
+                                color: 'white',
+                                height: '176px',
+                                background: 'black url(/Users/trym/go/src/github.com/Nosp1/Trym/portfolio/cvportfolio/logo.png) center / cover'
+                            }}>DnD: Character Generator</CardTitle>
+                            <CardText>
+                                StoryTeller is a open-source minimalistic character-creator right in your terminal for
+                                your favorite
+                                roleplaying game. The process of creating a character can be tedious and time consuming.
+                                Storyteller allows you to have a playable character within minutes. Save your character
+                                to JSON.
+                            </CardText>
+                            <CardActions style={{textAlign: 'center'}}>
+                                <Button colored href="https://github.com/Nosp1/Dnd" target="_blank">Github</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share"/>
+                            </CardMenu>
+                        </Card>
+                        {/* Project 2*/}
+                        <Card className="project-card" shadow={5}>
+                            <CardTitle className="card-title" style={{
+                                color: 'white',
+                                height: '176px',
+                                background: 'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80) center / cover',
+                            }}> Roombooking System</CardTitle>
+                            <CardText>
+                                A complete system that allows a user to book an available room in the system.
+                                The system allows users to easily book, rebook and keep track over their bookings.
+                                Features an admin-hub for easy management of the system and emailing-services to inform
+                                your clients.
+                            </CardText>
+                            <CardActions style={{textAlign: 'center'}}>
+                                <Button colored href="https://github.com/Nosp1/Gruppe10">Github</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share"/>
+                            </CardMenu>
+                        </Card>
+                    </div>
                 </div>
             )
         } else if (this.state.activeTab === 1) {
